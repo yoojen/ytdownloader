6217f5fcd03c719e9b9de1d48c110c0b48941afd
Eugene <eugenemutuyimana15@gmail.com>
2024-06-06 14:04:42 +0200

    implemented thumbnail navigation

--- Photo Gallery/index.js
+++ Photo Gallery/index.js
@@ -28,40 +28,12 @@ const images = [
     h2: "DEER",
     origin: "Amazon",
   },
-  {
-    id: 5,
-    src: "./images/wolf.webp",
-    h1: "COMMON BROWN",
-    h2: "BOBOON",
-    origin: "South Africa",
-  },
-  {
-    id: 6,
-    src: "./images/indian-spot-deer.jpg",
-
-    h1: "SPOTTED",
-    h2: "DEER",
-    origin: "Amazon",
-  },
-  {
-    id: 7,
-    src: "./images/fox.jpg",
-    h1: "FENNEC",
-    h2: "FOX",
-    origin: "India",
-  },
-  {
-    id: 8,
-    src: "./images/humpback-whale.jpg",
-    h1: "HUMPBACK",
-    h2: "WHALE",
-    origin: "South Africa",
-  },
 ];
 
 const container = document.querySelector(".container")
 const fullSizeModel = document.querySelector(".full-size-model")
 const closeBtn = document.querySelector(".close-btn");
+const thumbnailImgs = document.querySelector(".thumbnail_imgs");
 
 container.innerHTML = ""
 images.forEach((image) => {
@@ -78,6 +50,9 @@ images.forEach((image) => {
         </div>
       </div>
     `;
+    thumbnailImgs.innerHTML += `
+      <img src=${image.src} data-id = ${image.id} alt="">
+    `;
 })
 
 
@@ -88,13 +63,18 @@ imageArea.forEach((img) => {
     img.addEventListener("click", (e) => {
         fullSizeModel.style.display = "block";
         imageEl.src = e.target.src
-        imageEl.width = window.innerWidth -20;
+        imageEl.width = window.innerWidth;
         imageEl.height = window.innerHeight;
         imageEl.style.objectFit = "contain";
         fullSizeModel.append(imageEl)
     });
 })
 
+
+thumbnailImgs.addEventListener("click", (e) => {
+  imageEl.src = e.target.src
+  console.log(imageEl.src)
+})
 closeBtn.addEventListener("click", () => {
     fullSizeModel.removeChild(imageEl);
     fullSizeModel.style.display = "none";
