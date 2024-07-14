f72e577dc3959399610cb8945561d5c2a1aef96b
Eugene <eugenemutuyimana15@gmail.com>
2024-07-14 23:16:34 +0200

    download url registered

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import home
+from .views import home, download, show_available_download
 
 
 urlpatterns=[
-    path('home/', home, name="home")
+    path('home/', home, name="home"),
+    path('available-downloads/', show_available_download, name="show_available_download"),
+    path('download/', download, name="download"),
 ]
