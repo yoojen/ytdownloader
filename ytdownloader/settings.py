be3c10b0ef2f69bd189a163c1d153c305b5b476f
Eugene <eugenemutuyimana15@gmail.com>
2024-07-14 23:36:19 +0200

    added caching to support quick development

--- ytdownloader/settings.py
+++ ytdownloader/settings.py
@@ -82,6 +82,12 @@ DATABASES = {
     }
 }
 
+CACHES = {
+    "default": {
+        "BACKEND": "django.core.cache.backends.db.DatabaseCache",
+        "LOCATION": "cachhe_table",
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
