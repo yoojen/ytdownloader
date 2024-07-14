bfc83f1aebbcd43746d47d8d6be546998d173570
Eugene <eugenemutuyimana15@gmail.com>
2024-07-14 23:35:54 +0200

    audios and videos separated

--- core/views.py
+++ core/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
 from django.core.cache import cache
+from django.contrib import messages
 from pytube.contrib.search import Search
-from .utils import search_youtube_resources
-from .forms import SearchForm
-from .models import SearchQueries
+from .utils import request_youtube_resource, search_youtube_resources
+from .forms import SearchForm, DownloadForm
 
 def home(request):
     form = SearchForm()
@@ -26,6 +27,55 @@ def home(request):
                 thumbnail = d.thumbnail_url
                 rs_obj = {"title": title, "video_url": video_url, "thumbnail": thumbnail}
                 data.append(rs_obj)
-    print(data)
     return render(request=request, template_name="core/home.html", 
                   context={"form": form, "data": data})
+
+def show_available_download(request):
+    form = DownloadForm()
+    audios_dict = {}
+    videos_dict = {}
+    if request.method == "POST":
+        form = DownloadForm(request.POST)
+        try:
+            count = 0
+            if form.is_valid():
+                link = form.cleaned_data["yt_link"]
+                if cache.get(f"{link}_audios") or cache.get(f"{link}_videos"):
+                    print("from cache")
+                    audios = cache.get(f"{link}_audios")
+                    videos = cache.get(f"{link}_videos")
+                else:
+                    print("From API")
+                    audios, videos = request_youtube_resource(link)
+                    cache.set(f"{link}_audios", audios)
+                    cache.set(f"{link}_videos", videos)
+                for audio in audios:
+                    audios_dict[f"{count}_audio"] = {
+                        "title": audio.title,
+                        "type": audio.type,
+                        "size": audio.filesize_mb,
+                        "item": str(audio)
+                    }
+                    count = count + 1
+                for video in videos:
+                    videos_dict[f"{count}_video"] = {
+                        "title": video.title,
+                        "type": video.type,
+                        "size": video.filesize_mb,
+                        "item": str(video)
+                    }
+                    count = count + 1
+                return render(request, "core/download.html", {'form': form, 
+                                                              "audios": audios_dict, 
+                                                              "videos": videos_dict})
+        except Exception as e:
+            messages.error(request, str(e))
+            return redirect("show_available_download")
+
+    return render(request=request, template_name="core/download.html", context={'form': form,
+                                                                                "audios": audios_dict,
+                                                                                "videos": videos_dict})
+
+
+def download(request):
+    pass
