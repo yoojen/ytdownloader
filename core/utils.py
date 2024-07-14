00a4938329c98f171834c0dbc2cdea9dac068423
Eugene <eugenemutuyimana15@gmail.com>
2024-07-15 00:22:39 +0200

    refactored codes

--- core/utils.py
+++ core/utils.py
@@ -1,5 +1,6 @@
 from pytube import YouTube
 from pytube.contrib.search import Search
+from django.core.cache import cache
 
 def request_youtube_resource(yt_resource_link: str):
     """Module that send request to youtube api via pytube"""
@@ -20,3 +21,16 @@ def search_youtube_resources(request, query: str):
 
 def requested_next_res(request):
     next_rs = search_youtube_resources()
+
+def load_fetched_data(link):
+    if cache.get(f"{link}_audios") or cache.get(f"{link}_videos"):
+        audios = cache.get(f"{link}_audios")
+        videos = cache.get(f"{link}_videos")
+        thumbnail = cache.get(f"{link}_thumbnail")
+    else:
+        audios, videos, thumbnail_url = request_youtube_resource(
+            link)
+        cache.set(f"{link}_audios", audios)
+        cache.set(f"{link}_videos", videos)
+        thumbnail = cache.set(f"{link}_thumbnail", thumbnail_url)
+    return [audios, videos, thumbnail]
