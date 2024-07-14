286d53edca9dc3b3eb9b8ab4b39f20a88d1f3708
Eugene <eugenemutuyimana15@gmail.com>
2024-07-14 23:36:40 +0200

    fixed errors

--- core/utils.py
+++ core/utils.py
@@ -4,8 +4,10 @@ from pytube.contrib.search import Search
 def request_youtube_resource(yt_resource_link: str):
     """Module that send request to youtube api via pytube"""
     yt = YouTube(yt_resource_link)
-    rs = []
-    return rs
+    audios = yt.streams.filter(only_audio=True).order_by("abr")
+    videos = yt.streams.filter(file_extension='mp4')
+
+    return [audios, videos]
 
 
 def search_youtube_resources(request, query: str):
