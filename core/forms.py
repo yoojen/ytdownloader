0725256d4eb8642ae27b2baef806a07d48573ad7
Eugene <eugenemutuyimana15@gmail.com>
2024-07-14 23:16:00 +0200

    download form

--- core/forms.py
+++ core/forms.py
@@ -7,3 +7,13 @@ class DateInput(forms.DateInput):
 class SearchForm(forms.Form):
     query=forms.CharField(max_length=256)
     upload_at=forms.DateField(widget=DateInput(),required=False)
+
+class DownloadForm(forms.Form):
+    yt_link = forms.CharField(max_length=256, help_text="Paste link from youtube")
+
+    def clean_yt_link(self):
+        yt_link = self.cleaned_data["yt_link"]
+        if not yt_link.startswith("https://www.youtube.com/"):
+            raise ValueError("Provide valid youtbe link")
+        
+        return yt_link
